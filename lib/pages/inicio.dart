beafd33a2784c865c8c5f57493925e10ddb3a4f9
Janilso <janilsorodrigues10@gmail.com>
2019-09-28 10:15:47 -0300

    Tela Inicial iniciada

--- lib/pages/inicio.dart
+++ lib/pages/inicio.dart
@@ -6,10 +6,41 @@ class homePage extends StatefulWidget {
 }
 
 class _homePageState extends State<homePage> {
+  int _pontuacao = 250;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("Game"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.help_outline),
+              onPressed: () {},
+              color: Colors.white,
+            )
+          ],
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(200)),
+                ),
+                child: Center(
+                  child: Text(
+                    "$_pontuacao",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
